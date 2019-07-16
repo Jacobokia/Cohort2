5c6d7bf25baf9a83279fd03e54a1890043d8ef18
Irene Nyakate <inyakate@gmail.com>
2019-07-16 17:47:02 +0300

    Update irene.js

--- irene.js
+++ irene.js
@@ -1,15 +1,16 @@
+//Qn.1 Create one functio without parameters adding 4 numbers
 function addFourNumbers(){
     //function without parameters to add four numbers
     var number1 = 20;
-    // variable number1 with assigned value of 20
+    // declaring variable number1 and assigning it value of 20
     var number2 = 40;
-    // variable number2 with assigned value of 40
+    // declaring variable number2 and assigning it value of 40
     var number3 = 70;
-    // variable number3 with assigned value of 70
+    // declaring variable number3 and assigning it value of 70
     var number4 = 34;
-    // variable number4 with assigned value of 34
+    // declaring variable number4 and assigning it value of 34
     var total = number1 + number2 + number3 + number4;
-    // variable total assigned the result of adding the four variables
+    // declarign variable total and assigning the result of adding the four variables
     
     console.log("The total is: " + total);
     //printing the total value to the console
@@ -17,11 +18,12 @@ function addFourNumbers(){
     addFourNumbers();
     // calling the function "addFourNumbers" which adds the four variables
 
+// Qn.2 Function with four parameters
 function addFourNumberWithParameters(numberone, numbertwo, numberthree, numberfour){
     //function with four parameters to add four numbers
 
    var result = numberone + numbertwo + numberthree + numberfour;
-    // variable result assigned the sum of the four variables
+    // declaring variable result and assigning it the sum of the four variables
     
     console.log("The result is: " +result);
     //printing the total value to the console
@@ -29,6 +31,7 @@ function addFourNumberWithParameters(numberone, numbertwo, numberthree, numberfo
     addFourNumberWithParameters(7, 8, 9, 90);
     // calling the function "addFourNumberWithParameters" which adds the four parameters passed
 
+// Qn.3  Function to check for odd numbers between 50 and 100
 function printOddNumbers(){
     //function to print odd numbers between 50 and 100
     for (numberone = 50; numberone <= 100; numberone++){
