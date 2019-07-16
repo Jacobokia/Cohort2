b3e24cdcc6ec09c846a3310d3499fae257fa027b
Samson Arsanios <ssamseat@hotmail.com>
2019-07-16 20:20:39 +0300

    Update sam.js

--- sam.js
+++ sam.js
@@ -3,10 +3,16 @@
 2. funciton with parameters
 3. function to check odd numbers between 50-100
 */
+
 /* Samson */
 
-//1 . Function without parameters
+/*==================================================================================================================================
+1 . Function without parameters
+===================================================================================================================================*/
+
 function addFourNumbers() {   // declaring the function 
+    
+    //declaring and assigning the values to variables simultaneously
     var num1 = 3;
     var num2 = 4;
     var num3 = 3;
@@ -18,10 +24,14 @@ function addFourNumbers() {   // declaring the function
     // printing out the value total to the console
     console.log(total)                
 }
+    //calling the function
+addFourNumbers();
 
-//2 . Function with parameters
+/*==================================================================================================================================
+2 . Function with parameters
+===================================================================================================================================*/
 
-// declaring the function to
+// declaring the function with parameters
 function addFourNumbersWithParam(num1, num2, num3, num4) { 
 
     // declaring and assigning the variable total to the sum of the variables above
@@ -29,10 +39,12 @@ function addFourNumbersWithParam(num1, num2, num3, num4) {
     console.log(total)
 }
 
+// invoking the function and passing the arguments
 addFourNumbersWithParam(10, 15, 25, 10);
-addFourNumbers();
 
-//3 . Function to output odd numbers between 50-100
+/*=================================================================================================================================
+3 . Function to output odd numbers between 50-100
+=================================================================================================================================*/
 
 function addOddNumbers() {                    // function declaration
     for (var num = 50; num <= 100; num++) {   // iterating through the numbers between 50-100
