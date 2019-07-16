ebbf149af4d41fe5ecdd122604579673c4184f3b
Tim Mwandha <37672827+timothy-mwandha@users.noreply.github.com>
2019-07-16 20:07:29 +0300

    Create tim-mwandha.js

---
+++ tim-mwandha.js
@@ -0,0 +1,16 @@
+/* function without parameters, adding up two operands*/
+function sum()
+var numb1, numb2, numb3; //declaring variables
+{
+sum(numb1, numb2)=numb3
+}
+document.write sum()
+
+
+/*function with parameters, summing up operands*/
+function sum(a, b);
+let a=10, b=20; //declaring and initializing variables a and b.
+{
+sum(a+b);
+}
+document.write sum()
