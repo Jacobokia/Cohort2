b19cf325570744c83b750a248c40605a6f099065
OcittiKenneth <51134873+OcittiKenneth@users.noreply.github.com>
2019-07-16 17:35:20 +0300

    Update kenneth.js
    Functions for adding and checking for odd numbers between 50 -100

--- kenneth.js
+++ kenneth.js
@@ -1,1 +1,31 @@
 
+// //Adding four numbers without perimeters.
+function add() {
+    var number1 = 100;
+    var number2 = 200;
+    var number3 = 300;
+    var number4 = 400;
+    var result = number1 + number2 + number3 + number4;
+
+    console.log(result);
+}
+
+add()
+
+//Adding four numbers with perimeters.
+function add(a, b, c, d) {
+    var result = a + b + c + d;
+    console.log(result);
+}
+add(100, 200, 300, 400);
+
+//Checking for addOddNumber between 50-100.
+function addOddNumber() {
+    for (var odd = 50; odd <= 100; odd++) {
+        if (odd % 2 !== 0) {
+            console.log(odd);
+        }
+
+    }
+}
+addOddNumber();
