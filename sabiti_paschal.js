9ba1b5ffd7f9328446d5f4c69c138061623f310d
sabbiti isaac paschal <51475517+paschalsabiti@users.noreply.github.com>
2019-07-17 14:32:43 +0300

    More comments to add to the code.

--- sabiti_paschal.js
+++ sabiti_paschal.js
@@ -23,15 +23,18 @@ dcs()
 /*when invoking, the function isnt filled with the arguments*/
 
 
-/*A function that checks odd numbers from 50 to 100*/
-/*this is done using the for loop*/
-for(var a = 50; a<=100; a++){/*this is the condition of the loop, it looks for a variable from 50 too 100 and uses the increment operator to move systematically one by one up to 100 from 50*/
-if (a%2!==0){console.log(a)}/*this is the modulus oprator that checks for the evenness of a number by using the modulus. so if the number, when divided by 2 has a remainder of 1, then it is odd*/
-/*it has to print the value in the console*/
+/*A function that checks odd numbers from 50 to 100
+this is done using the for loop*/
+for(var a = 50; a<=100; a++){
+ /*this is the condition of the loop, it looks for a variable from 50 too 100 and uses the increment operator to move systematically one by one up to 100 from 50*/
+if (a%2!==0){console.log(a)}
+ /*this is the modulus oprator that checks for the evenness of a number by using the modulus. so if the number, when divided by 2 has a remainder of 1, then it is odd*/
+
+ /*it has to print the value in the console*/
 }
 
 
 
 /*this is sabbiti isaac paschal's work
 thank you very much.
-regards.*?*/
+regards.?*/
