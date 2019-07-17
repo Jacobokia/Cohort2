a5d6e82d685c7bc3d32e98a8667ce2198ec44d0a
FunaNyumba <52739256+FunaNyumba@users.noreply.github.com>
2019-07-17 09:39:41 +0300

    Create sabbiti_Isaac.js

---
+++ sabbiti_Isaac.js
@@ -0,0 +1,37 @@
+/*A function 'dsu' that has four parameters with arguments*/
+function dsu(a,b,c,d){
+/*the arguments are a,b,c and d*/
+var r = a+b+c+d;
+console.log(r);
+}
+dsu(1,2,3,4)
+/*the arguments are defined in the invocation of the function*/
+
+
+
+/*A function 'dcs' that has four aruguments without arguments*/
+function dcs(){
+/*the variables are listed and defined here*/
+var a = 1;
+var b = 2;
+var c = 3;
+var d = 4;
+var r = a+b+c+d;
+console.log(r)
+}
+dcs()
+/*when invoking, the function isnt filled with the arguments*/
+
+
+/*A function that checks odd numbers from 50 to 100*/
+/*this is done using the for loop*/
+for(var a = 50; a<=100; a++){/*this is the condition of the loop, it looks for a variable from 50 too 100 and uses the increment operator to move systematically one by one up to 100 from 50*/
+if (a%2!==0){console.log(a)}/*this is the modulus oprator that checks for the evenness of a number by using the modulus. so if the number, when divided by 2 has a remainder of 1, then it is odd*/
+/*it has to print the value in the console*/
+}
+
+
+
+this is sabbiti isaac paschal's work
+thank you very much.
+regards.
