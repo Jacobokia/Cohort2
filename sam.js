d7aa6609db3fc4593880a6f6495f2949021ddef0
Samson Arsanios <ssamseat@hotmail.com>
2019-07-17 14:30:29 +0300

    Update sam.js

--- sam.js
+++ sam.js
@@ -19,7 +19,7 @@ function addFourNumbers() {   // declaring the function
     var num4 = 5;
     
     // declaring and assigning the variable total to the sum of the variables above
-    total = num1 + num2 + num3 + num4 
+    var total = num1 + num2 + num3 + num4 
     
     // printing out the value total to the console
     console.log(total)                
@@ -35,7 +35,7 @@ addFourNumbers();
 function addFourNumbersWithParam(num1, num2, num3, num4) { 
 
     // declaring and assigning the variable total to the sum of the variables above
-    total = num1 + num2 + num3 + num4
+    var total = num1 + num2 + num3 + num4
     console.log(total)
 }
 // invoking the function(addFourNumbersWithParam) and passing the arguments
@@ -46,9 +46,13 @@ addFourNumbersWithParam(10, 15, 25, 10);
 =================================================================================================================================*/
 
 function addOddNumbers() {                    // function declaration
-    for (var num = 50; num <= 100; num++) {   // iterating through the numbers between 50-100
-        if (num % 2 !== 0) {                  // conditional statement to check whether the numbers are odd or not
-            console.log(num);                 // prints out the values
+    
+     // iterating through the numbers between 50-100
+    for (var num = 50; num <= 100; num++) {        
+        // conditional statement to check whether the numbers are odd or not
+        if (num % 2 !== 0) {         
+            // prints out the values
+            console.log(num);                 
         }
     }
 }
