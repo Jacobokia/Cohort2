1781883bcc842d62740bf831fc74fd05207a935e
zauja <38703407+zauja@users.noreply.github.com>
2019-07-17 10:29:54 +0300

    Add files via upload

---
+++ awebwa.js
@@ -0,0 +1,52 @@
+<script>
+
+    function add() {
+        var num1=2;
+        var num2=4;
+        var num3=6;
+        var num4=8;
+        var sum=num1+num2+num3+num4;
+       console.log(sum);
+       
+    }
+    add();
+
+
+
+    function arg(a, b, c, d) 
+    {
+  console.log(arguments[0]);
+  
+
+  console.log(arguments[1]);
+  
+
+  console.log(arguments[2]);
+
+  console.log(arguments[3]);
+  
+}
+
+arg(1, 2, 3, 4);
+
+
+
+
+    function odd() {
+       var x;
+
+       for(x = 50;x<=100;x++){
+       
+       if(x%2 != 0){
+       
+       console.log(x);
+       
+       }
+       
+       }
+            
+        }
+    
+    0dd();
+
+</script>
