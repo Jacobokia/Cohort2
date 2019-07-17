8b8c1f733c00377bcb7515f85d85757551698d58
stephen-donga <52744619+stephen-donga@users.noreply.github.com>
2019-07-17 08:14:42 +0300

    Update stephen edonga.js

--- stephen edonga.js
+++ stephen edonga.js
@@ -1,1 +1,34 @@
+//function without parameters.
+function add(){
+  var num1=12;
+  var num2=32;
+  var num3=7;
+  var num4=43;
+  var num5=num1+num2+num3+num4;
+  console.log(num5);
+}
+add();
+
+//funtion with parameters.
+function add(a,b,c,d){
+  var e=a+b+c+d;
+  console.log(e);
+  
+}
+add(19,8,23,1);
+
+
+//function with odd numbers from 50-100.
+function oddNumbers(){
+  for(var b=50; b<=100; b++)
+  {
+    if(b%2!=0){
+      console.log(b);
+    }
+  }
+  
+}
+oddNumbers();
+
+
 
