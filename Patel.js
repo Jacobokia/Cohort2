9be21eb17cce4c6f6fbe51f2b729656fe4d552ec
trillionguy <52960922+trillionguy@users.noreply.github.com>
2019-07-17 12:22:47 +0300

    Add files via upload

---
+++ Patel.js
@@ -0,0 +1,26 @@
+
+function sum(){ //This is function called sum
+var barca=1, manutd=2, tot=3,mancity=4;//Declaring and intialing varibles.
+var soccer=barca+manutd+tot+mancity;//variable soccer adding up the numbers
+document.write(soccer+"<br/>")//displaying the solution
+}
+sum() //calling the function sum
+
+
+function add(barca1,manutd2,tot3,mancity4){//Stating the function
+var league5= barca1+manutd2+tot3+mancity4;//variable league5 adding up the numbers
+document.write(league5+"<br/>")}//Displaying the solution
+add(1,2,3,4)// calling the function
+
+
+function odd(){
+for(d=50; d<100; d++) //forloop used for looping numbers between 50 and 100
+{
+if(d%2!=0)//checking if the numbers are odd or not
+{
+document.write(d+","); //displaying the solution
+}
+}
+}
+odd();
+ 
